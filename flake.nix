748ce1d308547345d67b6202366df01eed445b8c
Mystic-W <guillaume.boussus@gmail.com>
2026-05-20 16:48:37 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    helium = {
+      url = "github:schembriaiden/helium-browser-nix-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, helium, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.trollface = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      specialArgs = {
+        inherit helium;
+      };
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.extraSpecialArgs = {
+            inherit helium;
+          };
+
+          home-manager.users.mystic = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, lib, helium, ... }:
+
+{
+  home.username = "mystic";
+  home.homeDirectory = "/home/mystic";
+
+  # Use the same version as your NixOS system.stateVersion.
+  # Do not randomly change this later.
+  home.stateVersion = "25.11";
+
+  home.packages = [
+    pkgs.libxkbcommon
+    helium.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+
+  dconf.enable = true;
+
+  dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = [
+        "caps:unlock_on_press"
+      ];
+    };
+  };
+
+  home.file.".config/xkb/compat/caps_win".text = ''
+    partial xkb_compatibility "unlock_on_press" {
+      interpret Caps_Lock+AnyOfOrNone(all) {
+        action= LockMods(modifiers=Lock, unlockOnPress=true);
+      };
+    };
+   '';
+
+  home.file.".config/xkb/rules/evdev".text = ''
+    ! include %S/evdev
+
+    ! option = compat
+      caps:unlock_on_press = +caps_win(unlock_on_press)
+  '';
+
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "helium.desktop";
+      "text/xml" = "helium.desktop";
+      "application/xhtml+xml" = "helium.desktop";
+      "x-scheme-handler/http" = "helium.desktop";
+      "x-scheme-handler/https" = "helium.desktop";
+      };
+  };
+}
