14029e94d3eb698b51825cb411df914cdce1c642
Mystic-W <guillaume.boussus@gmail.com>
2026-05-21 12:13:00 -0500

    fix: battery percentage wrong type

--- home.nix
+++ home.nix
@@ -35,7 +35,7 @@
     "org/gnome/desktop/interface" = {
       # dark mode
       color-scheme = "prefer-dark";
-      show-battery-percentage = "true";
+      show-battery-percentage = true;
     };
   };
 
